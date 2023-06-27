cb2b46aa6836cf651f1fdd17996807d0c1940412
Aidan Kovacic <aidankovacic@gmail.com>
2023-06-26 23:29:17 -0400

    Initial Commit

---
+++ CampusCompass/CampusCompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusCompassApp.swift
+//  CampusCompass
+//
+//  Created by Aidan Kovacic on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CampusCompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
