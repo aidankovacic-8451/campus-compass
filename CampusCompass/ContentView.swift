b280ee338361a56b92ffa3e536d94bd993bbb660
Nicholas Andrews <naandrews02@gmail.com>
2023-06-28 13:57:09 -0400

    Welcome Screen iteration 1

--- CampusCompass/ContentView.swift
+++ CampusCompass/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        //This ZStack allows the background color to stay behind the other elements of the welcome screen
+        ZStack{
+            
+            //This sets the welcome screen background color
+            Color("AccentColor")
+                .ignoresSafeArea()
+            
+            //This VStack aligns all visible elements of welcome screen
+            VStack{
+                Spacer()
+                
+                //This imports the png file of the logo and aligns it
+                Image("ccoutline")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.leading, 100.0)
+                    
+                //This displays the welcome message to users
+                Text("Welcome to CampusCompass")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                
+            }
         }
-        .padding()
     }
 }
 
